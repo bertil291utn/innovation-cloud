2358256d0f33a1554a8eff18bc59644cdf72f797
bertil291utn <bertilt4@gmail.com>
2020-05-08 13:13:48 -0500

    Add signups as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'signups#new'
   get 'thanks' => 'pages#thanks'
   resources :signups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
