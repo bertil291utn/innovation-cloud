6603a5974912b1e74b84990db4c85ccc94a49170
bertil291utn <bertilt4@gmail.com>
2020-05-08 15:18:06 -0500

    Add create action

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,4 +2,19 @@ class SignupsController < ApplicationController
   def new
     @signup = Signup.new
   end
+
+  def create
+    @signup = Signup.new(signup_params)
+    if @signup.save
+      redirect_to thanks_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def signup_params
+    params.require(:signup).permit(:email)
+  end
 end
