1c3e4101b45f13cfa007fa0cdb0468ca1d08a6b6
bertil291utn <bertilt4@gmail.com>
2020-05-08 13:01:05 -0500

    Add all routes by default  - Thanks action in pages controller to show in html view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'thanks' => 'pages#thanks'
+  resources :signups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
